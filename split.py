2f283786e46a6812d903269a78892fded0e79b1e
Bogusz Jelinski <bogusz.jelinski@gmail.com>
2019-09-14 17:04:10 +0200

    split: first version

--- split.py
+++ split.py
@@ -1,63 +1,103 @@
 from cvxopt.glpk import ilp
 import numpy as np
 from cvxopt import matrix
-big_cost=1000
 
-def solve_split (fil, size, distances, demand, cabs): # new demand, old demand
-    fil.write ("\n\nsolve_split for %d" % (size))
-    if len(demand)==0:
-        fil.write("No demand, quiting.")
+big_cost=2500
+DEBUG=0
+n_stands=20 # number of stands
+f = open("c:\\Users\\dell\\split.txt", "w")
+
+# count the goal function of a solved model
+# nn: number of customers=cabs (balanced)
+# cost: cost matrix
+# res: result received from solver
+# show: debug
+# sum: from which value to begin summing
+# supply: current trips of taxis
+# demand: new demand from customers
+def count_sum(nn, cost, res, demand, supply, show, sum): ## nn is number of customers=cabs
+    for taxi in range(0,nn): 
+        for trip in range(0,nn): 
+            if res[nn*taxi+trip]==1:
+                if cost[taxi][trip] < big_cost:
+                    if show==1:
+                        f.write ("\ncab %d takes customer %d" % (supply[taxi][0], demand[trip][0])) # [0] is ID
+                    sum = sum + dist[supply[taxi][2]][demand[trip][1]]
+    return sum
+
+# filters out elements in range - it maybe 'from', 'to' or 'id' which is checked
+def filter (input, range, element, show, fil):
+    count=0
+    output=[]
+    for row in input:
+        if row[element] in range:
+            output.append(row)
+            if show==1:
+                fil.write("(%d,%d,%d)" % (row[0], row[1], row[2]))
+            count = count +1
+    return count, output
+
+# delivers random supply & demand
+def rand_list(size, show):
+    list = []
+    count=0
+    for i in range(0,size):
+        frm = np.random.randint(0,size) # ID, from, to; from not used at that moment in cabs, 'to' not used in cust
+        to = np.random.randint(0,size)
+        if (frm != to): #  drop if equals
+            list.append((count, frm, to))  # indices in Python start with 0
+            if show==1:
+                f.write("(%d,%d,%d)" % (count,frm,to))
+            count = count +1 # TODO: list.size ?
+    return count, list
+
+def show_list(fil, label, list):
+    fil.write ("\n%s:" % (label))
+    for id, frm, to in list:
+        fil.write("(%d,%d,%d)" % (id,frm,to))
+
+# this rutine splits a model into four smaller models and finds their solutions + fifth solutions 
+# for customers and cabs which have not been covered by these four previous ones
+def solve_split (fil, size, distances, demand, cabs, show): # new demand, old demand
+    if len(demand)==0 or len(cabs)==0:
+        if show==1: fil.write("No demand or supply, quiting.")
         return
-    if len(cabs)==0:
-        fil.write("No supply, quiting.")
-        return
+
     rest_cust=[] # a list of IDs which have not been served
     rest_cabs=[] # which have not been used
     total=0
     start=0
     split_size = int(size/4)
     while start<size:
-        split_demand = []
-        nb_cust=0
-        split_cabs = []   
-        nb_cabs=0
         r = range(start, start+ split_size)
         s=0
-        for d_id, d_frm, d_to in demand:
-            if d_frm in r:
-                split_demand.append((d_id, d_frm, d_to))
-                nb_cust = nb_cust +1
-        for c_id, c_frm, c_to in cabs:
-            if c_to in r:
-                split_cabs.append((c_id,c_frm,c_to))
-                nb_cabs = nb_cabs+1
-        
+        nb_cust, split_demand = filter(demand, r, 1, 0, fil) # 1: frm; 0: NEVER DEBUG
+        nb_cabs, split_cabs = filter(cabs, r, 2, 0, fil) # 2: to; 0: NEVER DEBUG
+              
         if nb_cust>0 and nb_cabs>0:
-            fil.write ("\n\nRange %d to %d:" % (start, start+split_size))
-            fil.write ("\nCustomers:")
-            for d_id, d_frm, d_to in split_demand:
-                f.write("(%d,%d,%d)" % (d_id,d_frm,d_to))
-            fil.write ("\nCabs:")
-            for c_id, c_frm, c_to in split_cabs:
-                f.write("(%d,%d,%d)" % (c_id,c_frm,c_to))
+            if show==1:
+                fil.write ("\n\nRange %d to %d:" % (start, start+split_size))
+                show_list (fil, "Customers", split_demand)
+                show_list (fil, "Cabs", split_cabs)
 
-            x4, c = solve(distances, split_demand, split_cabs)
-            if (nb_cabs > nb_cust): nb = nb_cabs  # checking max size for unbalanced scenarios
-            else: nb = nb_cust
+            nb, x4, c = solve(distances, split_demand, split_cabs)
             
-            for taxi in range(0,nb): # three cabs
-                for trip in range(0,nb): # four customers
+            # sum up the solution and pick up unsatisfied customers (and cabs)
+            for taxi in range(0,nb): 
+                for trip in range(0,nb): 
                     if x4[nb * taxi + trip]==1:
                         if c[taxi][trip] == big_cost: # big cost means customer not served
-                            if nb_cabs > nb_cust:
+                            if nb_cabs > nb_cust: # there is always just one side dissatisfied
                                 rest_cabs.append(split_cabs[taxi][0])
                             else: 
                                 rest_cust.append(split_demand[trip][0])
-                        else:
-                            f.write ("\ncab %d takes customer %d" % (split_cabs[taxi][0], split_demand[trip][0]))
+                        else: # no, it is served
+                            if show==1:
+                                fil.write ("\ncab %d takes customer %d" % (split_cabs[taxi][0], split_demand[trip][0]))
                             s = s + dist[split_cabs[taxi][2]][split_demand[trip][1]]
-            f.write ("\nSum: %d " % (s) )
-        elif nb_cust>0:
+
+            if show==1: fil.write ("\nSum: %d " % (s) )
+        elif nb_cust>0: # this range has not been sendt to solver, but there might have beed demand or supply
             for d_id, d_frm, d_to in split_demand:
                 rest_cust.append(d_id)
         else:
@@ -66,42 +106,21 @@ def solve_split (fil, size, distances, demand, cabs): # new demand, old demand
         start = start + split_size
         total = total +s
 
-    # now solve customers without a cab so far
-    fil.write ("\n\nRest:")
-    rest_demand =[]
-    rest_supply =[]
-    nb_cust=0
-    nb_cabs=0
-    fil.write ("\nCustomers:")
-    for d_id, d_frm, d_to in demand:
-        if d_id in rest_cust:
-            rest_demand.append((d_id, d_frm, d_to))
-            f.write("(%d,%d,%d)" % (d_id,d_frm,d_to))
-            nb_cust = nb_cust +1
-    fil.write ("\nCabs:")
-    for c_id, c_frm, c_to in cabs:
-        if c_id in rest_cabs:
-            rest_supply.append((c_id,c_frm,c_to))
-            f.write("(%d,%d,%d)" % (c_id,c_frm,c_to))
-            nb_cabs = nb_cabs +1
-
-    x5, c_table = solve(distances, rest_demand, rest_supply)
-
-    if (nb_cabs > nb_cust): 
-        nn = nb_cabs  # checking max size for unbalanced scenarios
-    else: 
-        nn = nb_cust
+    # now solve customers without a cab so far - the fifth run
+    if show==1:
+        fil.write ("\n\nUnserved customers and cabs without demand:")
+        fil.write ("\nCustomers:")
+    nb_cust, rest_demand = filter(demand, rest_cust, 0, DEBUG, fil)
+    if show==1: fil.write ("\nCabs:")
+    nb_cabs, rest_supply = filter(cabs, rest_cabs, 0, DEBUG, fil)
     
-    for taxi in range(0,nn): 
-        for trip in range(0,nn): 
-            if x5[nn*taxi+trip]==1:
-                if c_table[taxi][trip] < big_cost:
-                    f.write ("\ncab %d takes customer %d" % (rest_supply[taxi][0], rest_demand[trip][0])) # [0] is ID
-                    total = total + dist[rest_supply[taxi][2]][rest_demand[trip][1]]
-    f.write("\nTotal cost: %d" % (total))
+    nn, x5, c_table = solve(distances, rest_demand, rest_supply)
+   
+    return count_sum(nn, c_table, x5, rest_demand, rest_supply, DEBUG, total)
+    
 ################################################
 
-def solve (distances, demand, cabs): # new demand, old demand
+def calculate_cost (distances, demand, cabs):
     n = 0
     if (len(cabs) > len(demand)): n = len(cabs)  # checking max size for unbalanced scenarios
     else: n = len(demand)
@@ -113,8 +132,13 @@ def solve (distances, demand, cabs): # new demand, old demand
             cost[c_idx][d_idx] = distances[c_to][d_frm] 
             d_idx = d_idx +1
         c_idx = c_idx+1
-    
-    c=matrix(cost, tc='d')
+   
+    return n,cost
+##################################################
+
+def solve (distances, demand, cabs): # new demand, old demand
+    n, cost = calculate_cost (distances, demand, cabs)
+    c = matrix(cost, tc='d')
     # constraints
     arr = np.zeros((n*2, n*n)) 
     for i in range(0,n):
@@ -128,57 +152,61 @@ def solve (distances, demand, cabs): # new demand, old demand
     I=set(range(n*n))
     B=set(range(n*n))
     (status,x)=ilp(c,g.T,h,a,b,I,B)
-    return x, cost
-
+    return n, x, cost
 ################################################################
-n_stands=12 # number of stands
-n_cabs=0
-n_cust=0
+
+# heuristic - lowest cost method
+# n: size
+# c: cost
+def LCM(n, c):
+    temp=np.array(c) # copy the cost table
+    d=temp.flatten()
+    total_cost=0
+    for i in range(0,n):  # so many iterations we have in LCM 
+        elem = d.argmin(0) # 0 is first axis (there is just one)
+        if d[elem]<big_cost: # do not sum a dummy cost
+            total_cost += d[elem]
+        
+        row = int(elem/n)
+        col = elem - row*n
+        for j in range(0,n): 
+            d[n*row+j]=big_cost; # marking row with a high cost
+            d[j*n+col]=big_cost; # marking column 
+    return total_cost
+##############################
+
+## MAIN ##
 dist = np.zeros((n_stands,n_stands)) # distances
 # calculate distances
 for i in range(0,n_stands):
     for j in range(i,n_stands):
         dist[j][i]=j-i # simplification of distance - stop9 is closer to stop7 than to stop1
         dist[i][j]=dist[j][i] 
-f = open("c:\\Users\\dell\\split.txt", "w")
-new_demand = []
+
 # ID, from, to; see the PDF file with explaination
-f.write ("The total demand:")
-dropped =0 # we have to make corrections to the index if some cases are dropped
-for i in range(0,n_stands):
-    frm = np.random.randint(0,n_stands)
-    to = np.random.randint(0,n_stands)
-    if (frm != to): #  drop if equals
-        new_demand.append((n_cust, frm, to))  # indices in Python start with 0
-        f.write("(%d,%d,%d)" % (n_cust,frm,to))
-        n_cust = n_cust +1
+if DEBUG==1: f.write ("The total demand:")
+n_cust, new_demand = rand_list (n_stands, DEBUG)
+if DEBUG==1: f.write ("\nThe total supply:")
+n_cabs, current_trips = rand_list (n_stands, DEBUG)
 
-current_trips = []
-# ID, from, to; from not used at that moment
-f.write ("\nThe total supply:")
-for i in range(0,n_stands):
-    frm = np.random.randint(0,n_stands)
-    to = np.random.randint(0,n_stands)
-    if (frm != to): #  drop if equals
-        current_trips.append((n_cabs, frm, to))  # indices in Python start with 0
-        f.write("(%d,%d,%d)" % (n_cabs,frm,to))
-        n_cabs = n_cabs +1
+# optimal solution, not split into four
+nn, x, cost_table = solve(dist, new_demand, current_trips)
 
-x, cost_table = solve(dist, new_demand, current_trips)
+res = count_sum (nn, cost_table, x, new_demand, current_trips, DEBUG, 0)
 
-if (n_cabs > n_cust): 
-    nn = n_cabs  # checking max size for unbalanced scenarios
-else: 
-    nn = n_cust
-res=0
-for taxi in range(0,nn): 
-    for trip in range(0,nn): 
-        if x[nn*taxi+trip]==1:
-            if cost_table[taxi][trip] < big_cost:
-                f.write ("\ncab %d takes customer %d" % (current_trips[taxi][0], new_demand[trip][0])) # [0] is ID
-                res = res + dist[current_trips[taxi][2]][new_demand[trip][1]]
+f.write ("\n")
+if DEBUG==1: f.write ("Sum: ")
+f.write ("%d " % (res))
 
-f.write ("\nSum: %d " % (res) )
+# now split the model into four
 
-solve_split(f, n_stands, dist, new_demand, current_trips)
-f.close()
+total = solve_split(f, n_stands, dist, new_demand, current_trips, DEBUG)
+if DEBUG==1: f.write("\nSplit cost: ")
+f.write("%d " % (total))
+
+# just to be sure - count heuristic 
+if DEBUG==1: f.write ("\nLCM sum: ")
+lcm = LCM(nn, matrix(cost_table, tc='d'))
+f.write ("%d " % (lcm))
+
+f.close() 
